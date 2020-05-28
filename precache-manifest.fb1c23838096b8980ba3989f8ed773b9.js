fbd96144f04a02cd40b4c363cb44d545cb73520a
Applegrub <sidyukovmichail@list.ru>
2020-05-28 20:39:03 +0300

    Updates

---
+++ precache-manifest.fb1c23838096b8980ba3989f8ed773b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca0025972c87fe701d723e391c6bbc8",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "5a8d68dfcd5dfed57fa8",
+    "url": "/toDoList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b47790bdf30b2081b8e6",
+    "url": "/toDoList/static/js/2.261c63d7.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/toDoList/static/js/2.261c63d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8d68dfcd5dfed57fa8",
+    "url": "/toDoList/static/js/main.007ba036.chunk.js"
+  },
+  {
+    "revision": "c41b609558b163dda5e5",
+    "url": "/toDoList/static/js/runtime-main.9717ffb7.js"
+  }
+]);
